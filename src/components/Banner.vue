a5b64637e11debedaa1aadf3cbeb4d2617ca1ded
Pili Chanampe <mariadelpilarchanampe@gmail.com>
2021-12-08 16:05:02 -0300

    font size in mobile fixed

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,24 +1,39 @@
 <template>
   <v-container
     fluid
-    class="ma-0 pa-0 pt-15 d-flex flex-column p-relative"
+    class="ma-0 pa-0 pt-15 d-flex flex-column p-relative justify-end"
+    style="height: 94.6vh;"
     :class="$vuetify.theme.dark ? 'black' : ''"
   >
     <div
       class="
-        pr-5
-        mr-15
         p-absolute
         d-flex
         flex-column
-        align-self-end"
-      style="z-index: 1; font-size: 6rem;"
+        align-self-end
+        flex-wrap
+        p-width-complete
+        px-6
+      "
+      :class="$vuetify.breakpoint.mdAndUp ? 'mb-15' : 'mb-6'"
+        style="z-index: 1;"
+        :style="$vuetify.breakpoint.lgAndUp ? 'font-size: 6rem;' : 'font-size: 3rem;'"
     >
-      <span>GONZALO<span class="font-weight-bold primary--text">VICTORIA</span></span>
-      <p
-        style="font-size: 2rem;"
-        class="d-flex align-self-end"
-      >Guitarrista, compositor, investigador, docente</p>
+      <div
+        class="p-width-complete d-flex flex-wrap"
+        :class="$vuetify.breakpoint.smAndDown ? 'justify-center' : 'justify-end'"
+      >
+        <span class="text-start">
+          GONZALO
+        </span>
+        <span class="font-weight-bold primary--text text-end">
+          VICTORIA
+        </span>
+      </div>
+      <div
+        :class="$vuetify.breakpoint.smAndDown ? 'text-center' : 'text-end'"
+        :style="$vuetify.breakpoint.smAndUp ? 'font-size: 2rem;' : 'font-size: 1.4rem;'"
+      >Guitarrista, compositor, investigador, docente</div>
     </div>
     <v-img
       v-show="$vuetify.theme.dark"
@@ -62,3 +77,27 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.typewriter {
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: .15em solid orange; /* The typwriter cursor */
+  white-space: nowrap; /* Keeps the content on a single line */
+  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+  letter-spacing: .15em; /* Adjust as needed */
+  animation:
+    typing 3.5s steps(40, end),
+    blink-caret .75s step-end infinite;
+}
+
+/* The typing effect */
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: orange; }
+}
+</style>
