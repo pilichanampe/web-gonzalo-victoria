e06cd3dc7bc3091648ee62094de0ed827387234e
Pili Chanampe <mariadelpilarchanampe@gmail.com>
2021-12-08 21:43:37 -0300

    i18n config ready

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import './components/basecomponents/_globals';
+import messages from './assets/common/locales/locales';
 
 Vue.config.productionTip = false;
+Vue.use(VueI18n);
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages, // set locale messages
+  // en, // set locale messages
+  // fr, // set locale messages
+});
 
 new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
